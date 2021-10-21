7c6ef2df171194b665de63ea0ac95bf3d61f3136
Eugene <e.a.shemetovgmail.com>
2021-10-21 21:12:14 +0300

    add proper and sentence buttons

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 document.getElementById("upper-case").addEventListener("click", function() {
     let text = document.querySelector("textarea").value;
     document.querySelector("textarea").value =
-        text.toUpperCase();
-});
-
-document.getElementById("lower-case").addEventListener("click", function() {
+    text.toUpperCase();
+  });
+  
+  document.getElementById("lower-case").addEventListener("click", function() {
     let text = document.querySelector("textarea").value;
     document.querySelector("textarea").value =
-        text.toLowerCase();
-});
-
-
+    text.toLowerCase();
+  });
+  
+  
+  document.getElementById("proper-case").addEventListener("click", function() {
+    let text = document.querySelector("textarea").value;
+   document.querySelector("textarea").value =
+  text.replace(/(^|\s)\S/g, function(firstChar) {
+   return firstChar.toUpperCase()
+    })
+  });
+  
+  
+  document.getElementById("sentence-case").addEventListener("click", function() {
+    let text = document.querySelector("textarea").value;
+    document.querySelector("textarea").value =
+    text.replace(/.+?[\.\?\!](\s|$)/g, function (txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+  });
